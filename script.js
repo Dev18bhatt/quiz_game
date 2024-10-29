cadc4d28f3a549065a8a04ebd52fd23ff644d867
Dev18bhatt <bhattmanish1974@gmail.com>
2024-10-29 13:11:36 +0530

    written quiz app

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+
+
     const startBtn = document.getElementById('start-btn');
     const nextBtn = document.getElementById('next-btn');
     const restartBtn = document.getElementById('restart-btn');
-
     const questionContainer = document.getElementById('question-container');
     const questionText = document.getElementById('question-text');
-    const choiceList = document.getElementById('choices-list');
+    const choicesList = document.getElementById('choices-list');
     const resultContainer = document.getElementById('result-container');
     const scoreDisplay = document.getElementById('score');
-
-
     const questions = [
         {
             question: "What is the capital of France?",
@@ -32,55 +31,44 @@ document.addEventListener("DOMContentLoaded", () => {
             answer: "William Shakespeare",
         },
     ];
-
-    // variable to track the question list
-    let currentQuestionIndex = 0;
     let score = 0;
+    let questionIndex = 0;
 
-    startBtn.addEventListener("click", startQuiz);
+    startBtn.addEventListener('click', startQuiz);
+    function startQuiz() {
+        startBtn.classList.add('hidden');
+        resultContainer.classList.add('hidden');
+        questionContainer.classList.remove('hidden');
+        showQuestion();
+    }
+    function showQuestion() {
+        nextBtn.classList.add('hidden');
+        questionText.textContent = questions[questionIndex].question;
+        choicesList.innerHTML = "";
+        questions[questionIndex].choices.forEach((choice) => {
+            const li = document.createElement('li');
+            li.textContent = choice;
+            li.addEventListener('click', () => { checkAns(choice) });
+            choicesList.appendChild(li);
+        })
+    }
+    function checkAns(choice) {
+        const ans = questions[questionIndex].answer;
+        if (ans === choice) {
+            score++;
+        }
+        nextBtn.classList.remove('hidden');
+    }
     nextBtn.addEventListener('click', () => {
-        currentQuestionIndex++;
-        if (currentQuestionIndex < questions.length) {
+
+        questionIndex++;
+        if (questionIndex < questions.length) {
             showQuestion();
         }
         else {
             showResult();
         }
     });
-    restartBtn.addEventListener("click", () => {
-        currentQuestionIndex = 0;
-        score = 0;
-        resultContainer.classList.add('hiddent');
-        startQuiz();
-    })
-
-    function startQuiz() {
-        startBtn.classList.add("hidden");
-        resultContainer.classList.add('hidden');
-        questionContainer.classList.remove('hidden');
-        showQuestion();
-    }
-
-    function showQuestion() {
-        nextBtn.classList.add('hidden');
-        questionText.textContent = questions[currentQuestionIndex].question;
-        choiceList.innerHTML = ""; // clear previous choices.
-        // display all the choices.
-        questions[currentQuestionIndex].choices.forEach((choice) => {
-            const li = document.createElement('li');
-            li.textContent = choice;
-            li.addEventListener('click', () => checkAnswer(choice));
-            choiceList.appendChild(li);
-        })
-    }
-    function checkAnswer(choice) {
-
-        const correctAnswer = questions[currentQuestionIndex].answer;
-        if (correctAnswer === choice) {
-            score++;
-        }
-        nextBtn.classList.remove('hidden');
-    }
 
     function showResult() {
         questionContainer.classList.add('hidden');
@@ -88,6 +76,12 @@ document.addEventListener("DOMContentLoaded", () => {
         scoreDisplay.textContent = `${score} out of ${questions.length}`;
     }
 
-
+    restartBtn.addEventListener('click', () => {
+        score = 0;
+        questionIndex = 0;
+        questionContainer.classList.remove('hidden');
+        resultContainer.classList.add('hidden');
+        startQuiz();
+    })
 
 })
